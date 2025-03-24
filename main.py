50cb4d3a6e0da1eafbe8943e609dea00b2cd4339
salah <salah.hoddin@gmail.com>
2025-03-24 08:34:04 +0330

    remove api id&hash,use internal api

--- main.py
+++ main.py
@@ -3,34 +3,37 @@ from telethon.tl.custom.message import Message
 
 ### config information
 
-# Replace Your Api-ID And Api-Hash From my.Telegram.org
-api_id = int(input("Insert API_ID:"))
-api_hash=str(input("Insert API_HASH:"))
-# Replace the group ID you want messages forwarded to.
-Group_id = int(input("Insert GROUP ID (with -):"))
+# you can Replace Your Api-ID And Api-Hash From my.Telegram.org
+api_id = 29793455
+api_hash= "b5da6c4d2b72ee566b2451fcb91e4ee2"
+client = TelegramClient('ForwardBot', api_id, api_hash,
+                        proxy=("HTTP","127.0.0.1",8087))
+
+
+# Replace the group/user ID you want messages forwarded to.
+Group_id = int(input("Insert GROUP ID (with -) Or USER ID you want the messages to be sent to:"))
 
 ### config information
 
 
-client = TelegramClient('ForwardBot', api_id, api_hash)
 
 
 @client.on(events.NewMessage(incoming=True,func=lambda e: e.is_private))
 
 async def forward_messages(event : Message):
 
-   DataUser = await event.get_sender()
+   dataUser = await event.get_sender()
 
-   fist_name = DataUser.first_name
-   last_name = DataUser.last_name if DataUser.last_name else '' #don't show last name and return None
-   username = f"sender name:` {fist_name} {last_name} ` \nsender id: [{DataUser.id}](tg://user?id={DataUser.id})"
+   fist_name = dataUser.first_name
+   username = f"sender name:` {fist_name}` \nsender id: [{dataUser.id}](tg://user?id={dataUser.id})"
 
-   if event.message.media and hasattr(event.message.media, 'ttl_seconds') :#if event is have Self-Destructing Messages
+   # if event is have Self-Destructing Messages
+   if event.message.media and hasattr(event.message.media, 'ttl_seconds') :
 
-      SDM = getattr(event.message.media , 'ttl_seconds',None)
+      sDM = getattr(event.message.media, 'ttl_seconds', None)
 
-      if SDM!=None:
-         MediaSDM = await event.download_media(file = bytes)
+      if sDM!=None:
+         MediaSDM = await event.download_media()
 
          await client.send_file(entity=Group_id, caption=username, file=MediaSDM, parse_mode="Markdown") #send media without type
 
@@ -42,11 +45,12 @@ async def forward_messages(event : Message):
 
    else:
       msg : Message = await client.forward_messages(entity=Group_id, messages=event.message)
-
       await client.send_message(entity=Group_id, message=username ,reply_to=msg, parse_mode="Markdown")
 
 
 
 if __name__ == '__main__':
    client.start()
+   if client.is_connected():
+      print("self bot is connected.")
    client.run_until_disconnected()
